8e0c82b8edccb08b6f6378efd9697f89f84f60fe
Balázs Úr <balazs@urbalazs.hu>
2019-03-25 07:53:06 +0100

    Add Hungarian translation

---
+++ Locale/hu_HU/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'This plugin stores files to Amazon S3' => 'Ez a bővítmény fájlokat tárol az Amazon S3 felhőben',
+    'AWS S3 region' => 'AWS S3 régió',
+    'AWS S3 bucket' => 'AWS S3 tároló',
+    'AWS S3 objects prefix' => 'AWS S3 objektumelőtag',
+    'AWS Access Key ID' => 'AWS hozzáférésikulcs-azonosító',
+    'AWS Secret Key' => 'AWS titkos kulcs',
+    'AWS S3 custom options' => 'AWS S3 egyéni beállítások',
+);
